9f309a94b23e0963d56061d33050faaa2d423ebd
zevtyardt <zevtyardt>
2022-11-03 20:33:36 +0700

    upload via git cli

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -3,27 +3,9 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 
 const PATH = "../..";
-
-const scrapingant = async () => {
-  const req = await axios.get("https://scrapingant.com/proxies");
-
-  const $ = cheerio.load(req.data);
-  const dt = $("tr")
-    .toArray()
-    .map((v) => $(v).text().trim().split(/\n */));
-
-  return {
-    header: dt.shift(),
-    body: dt,
-    key: 2,
-  };
-};
-
-const socks_proxy_net = async () => {
-  const req = await axios.get("https://www.socks-proxy.net/");
-
-  const $ = cheerio.load(req.data);
-  const table = $(".table-striped");
+const extract_table = (selector, data) => {
+  const $ = cheerio.load(data);
+  const table = $(selector);
   const head = table
     .find("thead")
     .find("th")
@@ -46,15 +28,71 @@ const socks_proxy_net = async () => {
   };
 };
 
+// providers
+const scrapingant = async () => {
+  const req = await axios.get("https://scrapingant.com/proxies");
+
+  const $ = cheerio.load(req.data);
+  const dt = $("tr")
+    .toArray()
+    .map((v) => $(v).text().trim().split(/\n */));
+
+  return {
+    header: dt.shift(),
+    body: dt,
+    key: 2,
+  };
+};
+
+const socks_proxy_net = async () => {
+  const req = await axios.get("https://www.socks-proxy.net/");
+  return extract_table(".table-striped", req.data);
+};
+
+const free_proxy_list = async () => {
+  const req = await axios.get("https://free-proxy-list.net/");
+  const data = extract_table(".table-striped", req.data);
+  data.key = "HTTP";
+  return data;
+};
+
+const proxyscrape = async () => {
+  const data = [];
+  for (let proto of ["http", "socks4", "socks5"]) {
+    const req = await axios.get(
+      `https://api.proxyscrape.com/v2/?request=displayproxies&protocol=${proto}}&timeout=10000&country=all&ssl=all&anonymity=all`
+    );
+    const lines = req.data.split(/\r?\n/);
+    for (let line of lines) {
+      if (line.indexOf(":") < 0) continue;
+      data.push([...line.trim().split(/\s*:\s*/), proto.toUpperCase()]);
+    }
+  }
+  return {
+    header: ["Ip", "Port", "Protocol"],
+    body: data,
+    key: 2,
+  };
+};
+
 fs.mkdir(`${PATH}/csv`, () => {});
 
 const main = async () => {
+  const filtered = new Set();
   const outs = {};
-  for (let provider of [scrapingant, socks_proxy_net]) {
+  for (let provider of [
+    scrapingant,
+    socks_proxy_net,
+    free_proxy_list,
+    proxyscrape,
+  ]) {
     console.log(`> get_proxies from ${provider.name}`);
     const result = await provider();
     result.body.forEach((value) => {
-      const type = value[result.key].toLowerCase();
+      const type =
+        typeof result.key == "string"
+          ? result.key.toLowerCase()
+          : value[result.key].toLowerCase();
       if (!outs[type]) {
         outs[type] = {
           csv: fs.createWriteStream(
@@ -64,10 +102,12 @@ const main = async () => {
         };
         outs[type].csv.write(result.header.join(",") + "\n");
       }
-      outs[type].raw.write(
-        `${value[result.ip || 0]}:${value[result.port || 1]}\n`
-      );
+
+      const proxy = `${value[result.ip || 0]}:${value[result.port || 1]}`;
+      if (filtered.has(proxy)) return;
+      outs[type].raw.write(proxy + "\n");
       outs[type].csv.write(value.join(",") + "\n");
+      filtered.add(proxy);
     });
     console.log(`< done write ${result.body.length} proxies`);
   }
