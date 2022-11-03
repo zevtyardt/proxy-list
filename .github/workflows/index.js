75c3dbd6bfada5e683fe397677cc6da97df9eae5
zevtyardt <zevtyardt>
2022-11-03 13:18:14 +0700

    edit .github/workflows/auto-update.yml

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -2,7 +2,6 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require("fs");
 
-const out = fs.createWriteStream("../../proxy.csv");
 const main = async () => {
   const req = await axios.get("https://scrapingant.com/proxies");
 
@@ -11,8 +10,17 @@ const main = async () => {
     .toArray()
     .map((v) => $(v).text().trim().split(/\n */));
 
+  const header = dt.shift();
+  const outs = {};
+
   dt.forEach((value) => {
-    out.write(value.join(",") + "\n");
+    const type = value[2].toLowerCase();
+    if (!outs[type]) {
+      outs[type] = fs.createWriteStream(`.../../${type}_proxy.txt`);
+      outs[type].write(header.join(",") + "\n");
+    }
+
+    outs[type].write(value.join(",") + "\n");
   });
 };
 
